a0fe608330922dd03054c9c631dfdd284154e02c
Luis Angel rodriguez Hernandez <l.rodriguezhdz@hotmail.com>
2021-08-05 17:45:13 -0500

    post agregado

--- src/app/servicios/api/api.service.ts
+++ src/app/servicios/api/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {LoginI} from '../../modelos/Login.interface';
+import {ResponseI} from '../../modelos/responsive.interface';
+import { HttpClient,HttpHeaders} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  url:string ="http://api.solodata.es/";
+  constructor(private http:HttpClient) { }
+
+
+loginByEmail(form :LoginI):Observable<ResponseI>
+{
+  let direccion = this .url + "auth";
+  return this.http.post<ResponseI>(direccion,form);
 }
+
+}
+
--- src/app/vistas/login/login.component.ts
+++ src/app/vistas/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl, Validators } from '@angular/forms';
+import { ApiService } from '../../servicios/api/api.service';
+import { LoginI } from '../../modelos/Login.interface';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -11,13 +13,17 @@ export class LoginComponent implements OnInit {
    usuario: new FormControl('',Validators.required),
    password: new FormControl('',Validators.required)
   })
-  constructor() { }
+  constructor(private api:ApiService) {
+
+   }
 
   ngOnInit(): void {
   }
 
-  onLogin(form: any){
-    console.log(form)
+  onLogin(form: LoginI){
+    this.api.loginByEmail(form).subscribe(data =>{
+      console.log(data);
+    })
   }
 
 }
